2def198afebe5174256fe94ef814706b76044fd8
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-10-21 10:08:13 +0530

    day18 str methods@2

---
+++ day18.js
@@ -0,0 +1,55 @@
+// let a="customer@gmail.com"
+// console.log(a.startsWith("customer"))
+
+// let url="https://www.youtube.com";
+// console.log(url.startsWith("hr"))
+
+// let address="vamsi@gmail.com";
+// console.log(address.endsWith(".govt"))
+
+// let fileType="vamsiResume@10Kcoders.pdf";
+// console.log(fileType.endsWith(".pdf"))
+
+
+// let quote="javascript is so easy, but javascript is very tough"
+// console.log(quote.lastIndexOf("javascript"))
+
+// let coupon="save10 save20 save30 save10";
+// console.log(coupon.lastIndexOf("save10"))
+
+
+// var  content=document.getElementById("offer")
+
+// const offer="Offer!!!     ";
+// const times=offer.repeat(5)
+// content.innerHTML=times;
+
+// const a="in stock";
+// console.log(a)
+// setTimeout(function (){
+//     console.log(a.replace("in","out of"))
+// },10000)
+
+// const currency="10000Rs";
+// setTimeout(function () {
+//     console.log(currency.replace("Rs","$"))
+// }, 6000);
+
+// let products = ["Laptop", "Tablet", "Smartphone", "Smartwatch"];
+// let modify=products.slice(0,2);
+// // console.log(products)
+// let modify2=products.splice(3,"qwertyuio","rakesh","hello","rahul")
+// console.log(products)
+
+
+// const data=["watch","mobile","laptop","Led screens 55Inches"];
+// console.log(data.splice(2,1,"desktop","mouse"))
+  // w,m,d,l,led
+// splice(starting index, "no of elements to accessed","data to be added")
+
+
+// const a=10000;
+// console.log(typeof a.toString())
+
+// const a=["qwer","asdfgh"];
+// console.log(typeof a.toString());
